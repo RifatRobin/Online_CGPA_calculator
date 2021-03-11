96a9f83df218b44b6653231e0ce234063c28a99b
RifatRobin <rifatitna@gmail.com>
2021-03-11 21:55:38 +0600

    have to attend in many place so cannot work on it, having a rough time.  modified

--- OCC/views.py
+++ OCC/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render, redirect, HttpResponse
 # Create your views here.
 
 
-def login(request):
-    return render(request, "login.html")
+# def login(request):
+#     return render(request, "login.html")
+
+
+# def signup(request):
+#     return render(request, 'signup.html')
+
+
+def calculate(request):
+    return render(request, 'calculate.html')
