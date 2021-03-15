3c7bfe3647bbffc993253debc67b9c3dcd9b6043
RifatRobin <rifatitna@gmail.com>
2021-03-16 00:28:57 +0600

    it does not need any signup or signin page but i did it to use in other projects

--- OCC/urls.py
+++ OCC/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    # path('login', views.login, name="login"),
-    # path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
+    path('signup', views.signup, name="signup"),
     path('', views.calculate, name="calculate"),
 ]
